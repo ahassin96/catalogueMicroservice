cfaa8a0792ff52a094841686278245f70c48c621
Alistair Hassin <alistairh96@outlook.com>
2024-01-13 11:57:06 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+
+mongo_client = MongoClient("mongodb://ec2-54-221-90-30.compute-1.amazonaws.com:27017")
+database = mongo_client.admin
+genres = ['horror', 'military', 'action']
+
+@app.route('/movies', methods=['GET'])
+def get_movies():
+    try:
+        result = {}
+        for genre in genres:
+            videos = database.movies.find({'genre': genre})
+            result[genre] = []
+
+            for video in videos:
+                result[genre].append({
+                    'title': video['title'],
+                    'url': video['url'],
+                    '_id': str(video['_id']),
+                    'watch_details': f'http://3.90.74.38:5000/watch.php/{str(video["_id"])}'
+                })
+
+        return jsonify(result)
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
